3f69b37fd3dbd3d34303a5e074ec3f89bff1a314
J. Davis <98430977+0xjdavis@users.noreply.github.com>
2024-10-28 17:58:02 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,27 +1,29 @@
 import streamlit as st
 import pandas as pd
-import requests
 import plotly.express as px
+import requests
 
-# API call to get crypto data
+# Page config
+st.set_page_config(page_title='Crypto Dashboard', page_icon=':bar_chart:')
+
+# API call
 url = 'https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1&sparkline=false'
-crypto_data = requests.get(url).json()
+coins = requests.get(url).json()
+df = pd.DataFrame(coins)
 
-# Convert to dataframe  
-df = pd.DataFrame(crypto_data)
+# Top bar
+st.title('Crypto Dashboard')
+st.markdown('*Top 100 coins by market cap*')
 
-# Display as cards
-for i in range(0, len(df), 4):
-    col1, col2, col3, col4 = st.columns(4)
-    with col1:
-        st.header(df.iloc[i]['name'])
-        st.metric("Price", df.iloc[i]['current_price'], df.iloc[i]['price_change_percentage_24h'])
-    with col2:
-        st.header(df.iloc[i+1]['name'])
-        st.metric("Price", df.iloc[i+1]['current_price'], df.iloc[i+1]['price_change_percentage_24h'])  
-    with col3:
-        st.header(df.iloc[i+2]['name'])
-        st.metric("Price", df.iloc[i+2]['current_price'], df.iloc[i+2]['price_change_percentage_24h'])
-    with col4:
-        st.header(df.iloc[i+3]['name']) 
-        st.metric("Price", df.iloc[i+3]['current_price'], df.iloc[i+3]['price_change_percentage_24h'])
+# Coin selection
+selected_coin = st.selectbox('Select a coin', df['name'].unique())
+selected_df = df[df['name']==selected_coin]
+
+# Data display 
+col1, col2 = st.columns(2)
+col1.metric(label=selected_coin, value=f'${selected_df["current_price"].values[0]:,.2f}') 
+col2.metric(label='% Change (24hr)', value=f'{selected_df["price_change_percentage_24h"].values[0]:.2f}%')
+
+# Chart 
+fig = px.line(selected_df, x='market_cap_rank', y=["market_cap", "current_price"])
+st.plotly_chart(fig, use_container_width=True)
